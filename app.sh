6dce638cd606db9e7a9b119143cb0e9736f30829
Moekr <highter@outlook.com>
2017-12-19 16:22:48 +0800

    修改版本号

--- app.sh
+++ app.sh
@@ -5,7 +5,7 @@ path=/opt/blog
 # Application name.
 name=moekr-blog
 # Application version.
-version=1.0.1
+version=1.1.0
 
 # Application config file. Default config.yml
 config_file=${path}/config.yml
