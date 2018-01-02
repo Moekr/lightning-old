da311b46bc3bdd17ddbeac2d75813b722f55eb73
Moekr <highter@outlook.com>
2018-01-02 20:13:36 +0800

    修正设置默认值；增加判断是否显示邮箱；增加Google Analytics

--- src/main/java/com/moekr/blog/util/enums/Properties.java
+++ src/main/java/com/moekr/blog/util/enums/Properties.java
@@ -4,12 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum Properties {
-    LOGO("logo", "网站LOGO", "Moekr"),
-    NAME("name", "网站名称", "萌客"),
+    LOGO("logo", "网站LOGO", "BLOG"),
+    NAME("name", "网站名称", "博客"),
     EOF("eof", "文章结束标志", "--End Of File--"),
     COPYRIGHT("copyright", "文章版权声明", "未经许可禁止转载本文内容，转载请注明文章出处。"),
     FOOTER("footer", "页脚内容", "Powered by SpringFramework"),
-    EMAIL("email", "站长邮箱", "moekr@domain.com");
+    EMAIL("email", "站长邮箱", ""),
+    ANALYTICS("analytics", "Google Analytics标识", "");
 
     private String id;
     private String name;
